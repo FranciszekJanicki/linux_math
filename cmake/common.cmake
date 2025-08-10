c39cb8d11a66ae47b9b9d3c2c58a0f922745af6a
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-08-11 01:30:30 +0200

    changes

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -5,17 +5,3 @@ set(MAIN_DIR ${PROJECT_DIR}/main)
 set(CMAKE_DIR ${PROJECT_DIR}/cmake)
 set(COMPONENTS_DIR ${PROJECT_DIR}/components)
 set(SUBMODULES_DIR ${PROJECT_DIR}/submodules)
-
-set(CMAKE_EXPORT_COMPILE_COMMANDS TRUE)
-
-if(NOT CMAKE_PROJECT_NAME)
-    set(CMAKE_PROJECT_NAME project)
-endif()
-
-message(STATUS "Project name: ${CMAKE_PROJECT_NAME}")
-
-if(NOT CMAKE_BUILD_TYPE)
-    set(CMAKE_BUILD_TYPE "Debug")
-endif()
-
-message(STATUS "Build type: ${CMAKE_BUILD_TYPE}")
--- main/main.c
+++ main/main.c
@@ -1,34 +1,55 @@
-#include "allocator.h"
-#include "matrix.h"
+#include "matrix3.h"
+#include "transform3.h"
+#include "vector3.h"
 #include <stdio.h>
-#include <stdlib.h>
 
-static void* allocator_alloc(void* user, size_t buffer_size)
+void test_transform3()
 {
-    return malloc(buffer_size);
-}
+    matrix3_data_t rotation_array1[3][3] = {{1.0f, 0.0f, 0.0f},
+                                            {0.0f, 0.0f, -1.0f},
+                                            {0.0f, 1.0f, 0.0f}};
 
-static void allocator_dealloc(void* user, void* buffer)
-{
-    free(buffer);
+    vector3_data_t translation_array1[3] = {1.0f, 2.0f, 3.0f};
+
+    matrix3_data_t rotation_array2[3][3] = {{0.0f, 1.0f, 0.0f},
+                                            {-1.0f, 0.0f, 0.0f},
+                                            {0.0f, 0.0f, 1.0f}};
+
+    vector3_data_t translation_array2[3] = {-1.0f, 0.5f, 0.0f};
+
+    transform3_t t1, t2, composed, inverse;
+    vector3_t vec = {{1.0f, 0.0f, 0.0f}};
+    vector3_t transformed_vec;
+
+    transform3_fill_with_arrays(&t1, &rotation_array1, &translation_array1);
+    transform3_fill_with_arrays(&t2, &rotation_array2, &translation_array2);
+
+    transform3_compose(&t1, &t2, &composed);
+    transform3_inverse(&composed, &inverse);
+    transform3_vector_transformation(&composed, &vec, &transformed_vec);
+
+    printf("Transform 1:\n");
+    transform3_print(&t1, "\n");
+
+    printf("\nTransform 2:\n");
+    transform3_print(&t2, "\n");
+
+    printf("\nComposed transform:\n");
+    transform3_print(&composed, "\n");
+
+    printf("\nInverse of composed transform:\n");
+    transform3_print(&inverse, "\n");
+
+    printf("\nOriginal vector:\n");
+    vector3_print(&vec, "\n");
+
+    printf("\nTransformed vector:\n");
+    vector3_print(&transformed_vec, "\n");
 }
 
 int main(int argc, char const** argv)
 {
-    allocator_t allocator;
-    allocator_initialize(
-        &allocator,
-        &(allocator_interface_t){.user = NULL,
-                                 .alloc = allocator_alloc,
-                                 .dealloc = allocator_dealloc});
-
-    void* buffer = allocator_allocate(&allocator, 30);
-
-    snprintf(buffer, 30, "%s", "DUPA ZBITA\n");
-    printf("Buf type: %d", allocator.type);
-    allocator_deallocate(&allocator);
-
-    allocator_deinitialize(&allocator);
+    test_transform3();
 
     return 0;
 }
